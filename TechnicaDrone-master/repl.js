5f67c82232671fee68b2f46dab50ad956eb8d7eb
matthewharbour1 <mattharbour1@gmail.com>
2016-01-22 19:11:50 -0500

    Files for project

--- TechnicaDrone-master/repl.js
+++
@@ -1,9 +0,0 @@
-var args = {};
-process.argv.forEach(function (val, index, array) {
-	if (index < 2 || val == 'ip') return;
-	args = {ip: val};
-});
-
-var arDrone = require('ar-drone');
-var client  = arDrone.createClient(args);
-client.createRepl();
